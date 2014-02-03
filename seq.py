44205bfab35409f115459bffdccb6463a41adcc8
Eric Van Albert <eric@ericslaptop.(none)>
2014-02-02 20:37:07 -0500

    concurrency issues resolved

--- seq.py
+++ seq.py
@@ -9,6 +9,7 @@ class Sequencer(threading.Thread):
 		self.output_adapter=output_adapter
 		self.patterns={}
 		self.pat_id=0
+		self.lock=threading.Semaphore()
 
 	def add_pattern(self,pattern,start,stop=None):
 		my_id=self.get_next_id()
@@ -20,9 +21,17 @@ class Sequencer(threading.Thread):
 		return self.pat_id
 
 	def get_patterns(self):
+		self.lock.acquire()
+		v=self.patterns.values()
+		self.lock.release()
 		return self.patterns.values()
 
 	def remove_pattern(self,pat_id):
+		self.lock.acquire()
+		self.remove_pattern_unsafe()
+		self.lock.release()
+
+	def remove_pattern_unsafe(self,pat_id):
 		self.patterns[pat_id][2].remove()
 		self.patterns.pop(pat_id)
 
@@ -33,13 +42,15 @@ class Sequencer(threading.Thread):
 		self.go=True
 		while self.go:
 			t=self.timebase.get_tick()
+			self.lock.acquire()
 			ids_to_remove=[pat_id for (pat_id,(start,stop,p)) in self.patterns.iteritems() if stop is not None and stop < t]
 			for pat_id in ids_to_remove:
-				self.remove_pattern(pat_id)
+				self.remove_pattern_unsafe(pat_id)
 
 			for start,stop,p in self.patterns.values():
 				if start < t:
 					p.process(t-start)
+			self.lock.release()
 			time.sleep(0)
 
 class PrintingOutputAdapter:
