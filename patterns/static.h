8f3baafcc88e612c5806fc4f5cb2c8e0a8d8137d
Zach Banks <zbanks@mit.edu>
2015-06-08 02:04:11 -0400

    Merge back to master

---
+++ patterns/static.h
@@ -0,0 +1,16 @@
+#ifndef __PATTERNS_STATIC_H__
+#define __PATTERNS_STATIC_H__
+
+void pattern_init();
+void pattern_del();
+
+extern int n_patterns;
+extern pattern_t** patterns;
+
+extern pattern_t pat_full;
+extern pattern_t pat_wave;
+extern pattern_t pat_bubble;
+extern pattern_t pat_strobe;
+extern pattern_t pat_fade;
+
+#endif
