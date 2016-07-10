5288560d3cf15d9aa30063e714445319caebbdcf
Eric Van Albert <eric@van.al>
2016-07-10 17:02:38 -0400

    add vu

---
+++ resources/patterns/vu.0.glsl
@@ -0,0 +1,14 @@
+void main(void) {
+    vec2 uv = gl_FragCoord.xy / iResolution;
+    gl_FragColor = texture2D(iFrame, uv);
+
+    float SMOOTH = 0.01;
+
+    float lows = 1. - smoothstep(iAudioLow, iAudioLow + SMOOTH, abs(uv.x - 0.5));
+    float mids = 1. - smoothstep(iAudioMid, iAudioMid + SMOOTH, abs(uv.x - 0.5));
+    float his  = 1. - smoothstep(iAudioHi , iAudioHi  + SMOOTH, abs(uv.x - 0.5));
+
+    vec4 c = composite(composite(vec4(0., 0., 0.5, lows), vec4(0., 0., 1., mids)), vec4(0.3, 0.3, 1., his));
+    c.a *= iIntensity;
+    gl_FragColor = composite(gl_FragColor, c);
+}
