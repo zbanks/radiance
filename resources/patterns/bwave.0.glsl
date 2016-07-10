eec34af3c8b889913d4455a8e1f6b2f53ffed0d3
Zach Banks <zjbanks@gmail.com>
2016-07-10 03:47:46 -0400

    New patterns

--- resources/patterns/bwave.0.glsl
+++ resources/patterns/bwave.0.glsl
@@ -1,8 +1,6 @@
 void main(void) {
     vec2 uv = gl_FragCoord.xy / iResolution;
-    vec4 c = vec4(0., 0., 0., 1.);
     float x = (uv.x + uv.y) * 15 + iTime * 1;
-    c.a = iIntensity * (sin(x) / 2.0 + 0.5);
-
-    gl_FragColor = composite(texture2D(iFrame, uv), c);
+    gl_FragColor = texture2D(iFrame, uv);
+    gl_FragColor.a *= iIntensity * (sin(x) / 2.0 + 0.5);
 }
