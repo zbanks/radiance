356f4fb4dd334f428719871c9c1bf51252cddd8b
Eric Van Albert <eric@van.al>
2016-07-14 00:05:20 -0400

    add fire, fix wwave

--- resources/patterns/fire.0.glsl
+++ resources/patterns/fire.0.glsl
@@ -1,5 +1,17 @@
 void main(void) {
     vec2 uv = gl_FragCoord.xy / iResolution;
-    //TODO
-    gl_FragColor = vec4(1.0, 0.2, 0., 1.);
+    gl_FragColor = texture2D(iFrame, uv);
+
+    vec3 noise_input = vec3(uv * 3. + vec2(0., -iTime), iTime * 0.3);
+    vec2 shift = (vec2(noise(noise_input), noise(noise_input + 100.)) - 0.5);
+    shift += (vec2(noise(2. * noise_input), noise(2. * noise_input + 100.)) - 0.5) * 0.5;
+    shift += (vec2(noise(4. * noise_input), noise(4. * noise_input + 100.)) - 0.5) * 0.25;
+    shift = (iIntensity * 0.5 + 0.5) * shift + vec2(0., 0.5 - 0.5 * iIntensity);
+
+    uv = uv + shift;
+    vec4 color = vec4(1., uv.y, 0., smoothstep(0.2, 0.5, (1. - uv.y)));
+    
+    color.a *= smoothstep(0., 0.2, iIntensity);
+    
+    gl_FragColor = composite(gl_FragColor, color);
 }
--- resources/patterns/wwave.0.glsl
+++ resources/patterns/wwave.0.glsl
@@ -4,5 +4,5 @@ void main(void) {
     float xfreq = (iIntensity + 0.5) * 2.;
     float x = mod(uv.x * xfreq + xpos, 1.);
     gl_FragColor = texture2D(iFrame, uv);
-    gl_FragColor = composite(gl_FragColor, vec4(1., 1., 1., step(x, 0.5) * smoothstep(0., 0.5, iIntensity)));
+    gl_FragColor = composite(gl_FragColor, vec4(1., 1., 1., step(x, 0.3) * smoothstep(0., 0.5, iIntensity)));
 }
