b8a028c4ddcce87cdcfbb29af1ee233dcdb3af5b
Zach Banks <zjbanks@gmail.com>
2016-07-17 16:05:38 -0400

    Deck sets

---
+++ resources/patterns/stripey.0.glsl
@@ -0,0 +1,9 @@
+void main(void) {
+    vec2 uv = gl_FragCoord.xy / iResolution;
+    float xv = round(uv.x * 20); 
+    gl_FragColor = texture2D(iChannel[0], uv);
+    gl_FragColor.a *= exp(-iIntensity / 20.);
+
+    if (rand(vec2(xv, iTime)) < exp(-iIntensity * 4.))
+        gl_FragColor = texture2D(iFrame, uv);
+}
