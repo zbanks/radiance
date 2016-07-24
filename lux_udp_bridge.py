7541e0bd8a60a8cb4163d1dafc09db573eaea0e3
Eric Van Albert <eric@van.al>
2016-07-24 19:50:52 -0400

    actually get rid of lag

--- lux_udp_bridge.py
+++ lux_udp_bridge.py
@@ -1,42 +1,89 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import select
 import serial
 import socket
+import traceback
+import fcntl
+import os
+import select
+import time
+
+PACKET_SIZE = 1100
+MAX_BUF_LEN = PACKET_SIZE * 10
+DEBUG = False
 
 def run_lux_udp(host, port, dev):
     with serial.Serial(dev, baudrate=3000000, xonxoff=False) as ser:
+        fl = fcntl.fcntl(ser.fileno(), fcntl.F_GETFL)
+        fcntl.fcntl(ser.fileno(), fcntl.F_SETFL, fl | os.O_NONBLOCK)
+
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
         sock.bind((host, port))
+        sock.setblocking(0)
         last_addr = None
-        serial_buffer = ""
-
-        epoll = select.epoll()
-        epoll.register(sock.fileno(), select.EPOLLIN)
-        epoll.register(ser.fileno(), select.EPOLLIN)
+        ser_to_sock = []
+        sock_to_ser = b""
+        incomplete_buffer = b""
 
         while True:
-            events = epoll.poll()
-            if not events: continue
-            fds = zip(*events)[0]
-            if sock.fileno() in fds:
-                packet, last_addr = sock.recvfrom(1100)
-                #print ">", repr(packet)
-                if len(packet) == 0: # Ping, respond back
-                    sock.sendto("", 0, last_addr)
-                else:
-                    ser.write(packet)
-            if ser.fileno() in fds:
-                serial_buffer += ser.read()
-                while "\0" in serial_buffer:
-                    packet, null, serial_buffer = serial_buffer.partition("\0")
-                    sock.sendto(packet + null, 0, last_addr)
-                    #print "<", repr(packet)
+            inputs, outputs, errors = select.select([sock.fileno(), ser.fileno()], ([sock.fileno()] if len(ser_to_sock) else []) + ([ser.fileno()] if len(sock_to_ser) else []), [])
+
+            if sock.fileno() in inputs:
+                while True:
+                    try:
+                        packet, last_addr = sock.recvfrom(PACKET_SIZE)
+                    except BlockingIOError:
+                        break
+                    if len(packet) == 0: # Ping, respond back
+                        ser_to_sock.append((b"", last_addr))
+                    else:
+                        if len(sock_to_ser) < MAX_BUF_LEN:
+                            sock_to_ser += packet
+                    if DEBUG:
+                        print("UDP>", packet)
+
+            if ser.fileno() in inputs:
+                while True:
+                    try:
+                        packet = os.read(ser.fileno(), PACKET_SIZE)
+                    except BlockingIOError:
+                        break
+                    if len(packet) == 0:
+                        break
+                    if DEBUG:
+                        print("SER>", packet)
+                    incomplete_buffer += packet
+                    while b'\0' in incomplete_buffer:
+                        pkt, sep, incomplete_buffer = incomplete_buffer.partition(b'\0')
+                        ser_to_sock.append((pkt + sep, last_addr))
+
+            if sock.fileno() in outputs:
+                while len(ser_to_sock):
+                    try:
+                        pkt, addr = ser_to_sock[0]
+                        sock.sendto(pkt, 0, addr)
+                        if DEBUG:
+                            print("UDP<", pkt)
+                        ser_to_sock = ser_to_sock[1:]
+                    except BlockingIOError:
+                        break
+
+            if ser.fileno() in outputs:
+                while len(sock_to_ser):
+                    try:
+                        l = os.write(ser.fileno(), sock_to_ser)
+                        if DEBUG:
+                            print("SER<", sock_to_ser[0:l])
+                        sock_to_ser = sock_to_ser[l:]
+                    except BlockingIOError:
+                        break
 
 if __name__ == "__main__":
     while True:
         try:
             run_lux_udp(host="0.0.0.0", port=1365, dev="/dev/ttyACM0")
-        except Exception as e:
-            print e
-            select.select([], [], [], 5)
+        except Exception:
+            traceback.print_exc()
+            time.sleep(5)
+        except KeyboardInterrupt:
+            break
