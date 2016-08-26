5a8afd3121cea463f943f72f15424f3cd40cbb84
Allan Sadun <sadun@localhost.localdomain>
2016-08-26 01:38:29 -0400

    add OpenGL 2.1 compatibility

--- ui/ui.c
+++ ui/ui.c
@@ -187,8 +187,8 @@ void ui_init() {
     // Init SDL
     if(SDL_Init(SDL_INIT_VIDEO) < 0) FAIL("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
 
     ww = config.ui.window_width;
     wh = config.ui.window_height;
