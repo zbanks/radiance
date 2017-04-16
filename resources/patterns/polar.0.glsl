e8f8ef4e6a23aa3b021423cfd607acac1d88801b
Zach Banks <zjbanks@gmail.com>
2017-04-16 11:30:22 -0400

    More radial patterns (depolar, polygon, flower)

--- resources/patterns/polar.0.glsl
+++ resources/patterns/polar.0.glsl
@@ -3,8 +3,9 @@
 void main(void) {
     vec2 xy = gl_FragCoord.xy / iResolution;
     vec2 xy_cent = 2. * xy - 1.;
-
-    vec2 uv = vec2(mix(xy.x, length(xy_cent) / sqrt(2.), iIntensity), mix(xy.y, abs(atan(xy_cent.x, -xy_cent.y) / M_PI), iIntensity));
+    float angle = atan(xy_cent.y, xy_cent.x);
+    vec2 rtheta = vec2(length(xy_cent) / sqrt(2.), 0.5 + angle / (2. * M_PI));
+    vec2 uv = mix(xy, rtheta, iIntensity);
 
     gl_FragColor = texture2D(iFrame, uv);
 }
