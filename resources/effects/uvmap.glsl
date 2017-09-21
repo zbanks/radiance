7e2277fddd53acfe11fbad19213e84ec35d117c7
gdkar <gdkar@alum.mit.edu>
2017-09-20 22:30:55 -0400

    re-add uvmap

---
+++ resources/effects/uvmap.glsl
@@ -0,0 +1,8 @@
+// Use .rg as .uv
+
+#property inputCount 2
+void main(void) {
+    vec4 map = texture2D(iInputs[1], uv);
+    vec2 newUV = mix(uv, map.rg, iIntensity);
+    gl_FragColor = texture2D(iInput, newUV);
+}
