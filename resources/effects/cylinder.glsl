41add36188132102a893ed60fa4e8e86a2649606
Zach Banks <zjbanks@gmail.com>
2017-10-14 17:53:02 -0400

    New effect: cylinder

---
+++ resources/effects/cylinder.glsl
@@ -0,0 +1,14 @@
+// Wrap the parent texture on a spinning cylinder
+
+void main(void) {
+    float angle = iIntensityIntegral * 32.0;
+    angle += 2 * asin(2 * (uv.x - 0.5));
+
+    float x = mod(angle / (M_PI), 2.0);
+    x -= 1.0;
+    x = abs(x);
+
+    vec2 new_uv = vec2(x, uv.y);
+    new_uv = mix(uv, new_uv, smoothstep(0., 0.15, iIntensity));
+    gl_FragColor = texture2D(iInput, new_uv);
+}
