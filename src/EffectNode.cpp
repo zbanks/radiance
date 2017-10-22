4b7b8b477ad284ba44413770ff007ac3e1e350da
Eric Van Albert <eric@van.al>
2017-10-21 22:13:19 -0400

    Make 3.2 core work

--- src/EffectNode.cpp
+++ src/EffectNode.cpp
@@ -5,6 +5,7 @@
 #include <QOpenGLFramebufferObject>
 #include <QOpenGLFramebufferObjectFormat>
 #include <QRegularExpression>
+#include <QOpenGLVertexArrayObject>
 #include <memory>
 #include <utility>
 #include <functional>
@@ -223,6 +224,10 @@ GLuint EffectNode::paint(QSharedPointer<Chain> chain, QVector<GLuint> inputTextu
             p->setUniformValue("iNoise", m_inputCount);
             p->setUniformValue("iResolution", GLfloat(size.width()), GLfloat(size.height()));
             p->setUniformValueArray("iChannel", &chanTex[0], m_programs.size());
+
+            QOpenGLVertexArrayObject vao;
+            vao.create();
+            vao.bind();
             glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
             renderState->m_intermediate.at(fboIndex)->release();
             //renderState->m_intermediate.at(fboIndex)->toImage().save(QString("out_%1.png").arg(renderState->m_intermediate.at(fboIndex)->texture()));
--- src/MovieNode.cpp
+++ src/MovieNode.cpp
@@ -4,6 +4,7 @@
 #include <QFile>
 #include <QFileInfo>
 #include <QOpenGLFramebufferObjectFormat>
+#include <QOpenGLVertexArrayObject>
 #include "main.h"
 
 MovieNode::MovieNode()
@@ -291,6 +292,9 @@ GLuint MovieNode::paint(QSharedPointer<Chain> chain, QVector<GLuint> inputTextur
         m_blitShader->setUniformValue("iVideoFrame", 0);
         m_blitShader->setUniformValue("iResolution", GLfloat(chain->size().width()), GLfloat(chain->size().height()));
         m_blitShader->setUniformValue("iVideoResolution", GLfloat(m_videoSize.width()), GLfloat(m_videoSize.height()));
+        QOpenGLVertexArrayObject vao;
+        vao.create();
+        vao.bind();
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
     }
     m_blitShader->release();
@@ -505,7 +509,6 @@ MovieNodeOpenGLWorker::~MovieNodeOpenGLWorker() {
 bool MovieNodeOpenGLWorker::loadBlitShader() {
     auto vertexString = QString{
         "#version 150\n"
-        "in int gl_VertexID;\n"
         "out vec2 coords;\n"
         "const vec2 varray[4] = vec2[](vec2(1., 1.),vec2(1., -1.),vec2(-1., 1.),vec2(-1., -1.));\n"
         "void main() {\n"
