ba11c8b747b5332c8aaa62df39b2f217c03782fe
gdkar <gdkar@alum.mit.edu>
2017-10-21 22:37:48 -0400

    scope your temporaries; upgrade standaline

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@
 
 #ifdef RTMIDI_FOUND
 #include "MidiController.h"
-#endif 
+#endif
 
 #ifdef MPV_FOUND
 #include "MovieNode.h"
@@ -52,15 +52,16 @@ int main(int argc, char *argv[]) {
     QCoreApplication::setOrganizationName("Radiance");
     QCoreApplication::setOrganizationDomain("radiance.lighting");
     QCoreApplication::setApplicationName("Radiance");
-    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
 
     // Use OpenGL 3.2 core profile
     // (otherwise MacOS falls back to 2.1)
-    QSurfaceFormat format = QSurfaceFormat::defaultFormat();
-    format.setVersion(3, 2);
-    format.setProfile(QSurfaceFormat::CoreProfile);
-    QSurfaceFormat::setDefaultFormat(format);
-
+    {
+        auto format = QSurfaceFormat::defaultFormat();
+        format.setVersion(3, 2);
+        format.setProfile(QSurfaceFormat::CoreProfile);
+        QSurfaceFormat::setDefaultFormat(format);
+    }
+    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
     QGuiApplication app(argc, argv);
 
     QThread::currentThread()->setObjectName("mainThread");
--- src/main_standalone.cpp
+++ src/main_standalone.cpp
@@ -47,6 +47,12 @@ int main(int argc, char *argv[]) {
     QCoreApplication::setOrganizationName("Radiance");
     QCoreApplication::setOrganizationDomain("radiance.lighting");
     QCoreApplication::setApplicationName("Radiance");
+    {
+        auto format = QSurfaceFormat::defaultFormat();
+        format.setVersion(3, 2);
+        format.setProfile(QSurfaceFormat::CoreProfile);
+        QSurfaceFormat::setDefaultFormat(format);
+    }
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
     QGuiApplication app(argc, argv); // TODO: how to avoid this?
 
