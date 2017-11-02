0ad2bef6f56fb3c1b0c22d1dc753732118518340
Zach Banks <zjbanks@gmail.com>
2017-11-01 20:02:28 -0400

    New effects: bespeckle, bespecklep

---
+++ resources/library/effects/bespecklep.glsl
@@ -0,0 +1,13 @@
+#property description Brownian-ish speckle effect with perlin noise
+
+void main(void) {
+    vec4 old = texture(iChannel[0], uv);
+    vec4 new = texture(iInput, uv);
+
+    float r = noise(vec3(uv * 16., iTime * 16.));
+    float k = pow(mix(1.0, r, iIntensity), 2.0);
+    fragColor = mix(old, new, k);
+
+    // I don't think this is required, but just be safe
+    fragColor.rgb = clamp(fragColor.rgb, 0.0, fragColor.a);
+}
