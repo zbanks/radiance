8475f4232382928ee068c6c73b31923e1e9a6fd9
Zach Banks <zjbanks@gmail.com>
2017-11-06 21:37:02 -0500

    New effect: loopy (still WIP; will be tomorrow's NaShaWriMo)

---
+++ resources/library/effects/loopy.glsl
@@ -0,0 +1,36 @@
+#property description Loopy laser-like pattern
+
+void main(void) {
+    vec4 originalColor = texture(iInput, uv);
+    vec2 normCoord = (uv - 0.5) * aspectCorrection;
+
+    // Decompose coordinate into R-theta
+    float r = length(normCoord);
+    float angle = atan(normCoord.x, normCoord.y);
+
+    // Compute the size/position of the 'bulge' that travels around the circle
+    float BULGE_SPEED = 1. / 8.0;
+    float BULGE_WIDTH = 0.25;
+    float BULGE_HEIGHT = 0.03;
+    float NOBULGE_HEIGHT = 0.01;
+    float angle_t = mod(angle / M_PI + iTime * BULGE_SPEED, 2.0) - 1.0;
+    float bulge = exp(-pow(angle_t / BULGE_WIDTH, 2.)) * BULGE_HEIGHT;
+    bulge += NOBULGE_HEIGHT;
+
+    // Compute distance from circle, including bulge & standing wave
+    float CIRCLE_R = 0.33;
+    float WAVE_COUNT = 16.;
+    float WAVE_SPEED = 1 / 4.0;
+    float d = abs(r - CIRCLE_R - bulge * sin(angle * WAVE_COUNT - iTime * M_PI * WAVE_SPEED));
+
+    // Turn distance into alpha 
+    float THICKNESS = 0.1;
+    float alpha = max(0.0, 1.0 - 10. * d);
+    alpha = max(0., cos(min(1.8, (1.0 - alpha) / THICKNESS)));
+
+    // Composite purple onto input
+    vec4 color = vec4(0.8, 0.1, 0.9, 1.0);
+    color *= alpha;
+    color *= smoothstep(0.0, 0.2, iIntensity);
+    fragColor = composite(originalColor, color);
+}
