037c8022b9e267e0b70f836fc2f76ed6c61b4f16
Eric Van Albert <eric@van.al>
2017-11-06 19:57:12 -0500

    Demo formatting convention for @gdkar

--- src/ProbeReg.cpp
+++ src/ProbeReg.cpp
@@ -5,32 +5,30 @@
 
 std::atomic<ProbeRegistry *> ProbeRegistry::s_head{};
 
-const ProbeRegistry *ProbeRegistry::head()
-{
+const ProbeRegistry *ProbeRegistry::head() {
     return s_head.load();
 }
-QList<NodeType *> ProbeRegistry::probeAll(NodeRegistry * nr)
-{
+
+QList<NodeType *> ProbeRegistry::probeAll(NodeRegistry * nr) {
     auto res = QList<NodeType*>{};
-    for(auto r = head(); r; r = r->next()) {
+    for (auto r = head(); r; r = r->next()) {
         res.append(r->probe(nr));
     }
     return res;
 }
-QList<NodeType *> ProbeRegistry::probe(NodeRegistry *) const
-{
+
+QList<NodeType *> ProbeRegistry::probe(NodeRegistry *) const {
     return {};
 }
-const ProbeRegistry *ProbeRegistry::next() const
-{
+
+const ProbeRegistry *ProbeRegistry::next() const {
     return m_next.load();
-}
-ProbeRegistry *ProbeRegistry::next()
-{
-    return m_next.load();
-}
-ProbeRegistry::ProbeRegistry()
-: m_next(s_head.exchange(this))
-{
 }
 
+ProbeRegistry *ProbeRegistry::next() {
+    return m_next.load();
+}
+
+ProbeRegistry::ProbeRegistry()
+    : m_next(s_head.exchange(this)) {
+}
