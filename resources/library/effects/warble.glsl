f6773f0f0d3b1e4d1da44e3ae71c77296ac70cc1
Eric Van Albert <eric@van.al>
2017-11-09 23:07:15 -0500

    Make warble work on nvidia and have aspect correction

--- resources/library/effects/warble.glsl
+++ resources/library/effects/warble.glsl
@@ -1,9 +1,9 @@
 #property description Makes the image warbly
 
 void main(void) {
-    vec2 newPt = uv - 0.5;
+    vec2 newPt = (uv - 0.5) * aspectCorrection;
 
-    float bins = iIntensity * 10.;
+    float bins = max(iIntensity * 10., 1.);
 
     vec2 newPtInt = floor(newPt * bins);
     vec2 newPtFrac = fract(newPt * bins);
@@ -12,5 +12,5 @@ void main(void) {
     vec2 displacement = pow(abs(newPtFrac), vec2(1. + 2. * iIntensity)) * sign(newPtFrac);
     newPt = (newPtInt + 0.5 * displacement + 0.5) / bins;
 
-    fragColor = texture(iInput, newPt + 0.5);
+    fragColor = texture(iInput, newPt / aspectCorrection + 0.5);
 }
