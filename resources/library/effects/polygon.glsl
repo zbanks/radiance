e926985fd470e4a2202ee9504083c0117796d326
Eric Van Albert <eric@van.al>
2017-11-12 18:53:55 -0500

    add release cflags, remove polygon singularity and adjust clipping

--- resources/library/effects/polygon.glsl
+++ resources/library/effects/polygon.glsl
@@ -3,7 +3,7 @@
 void main(void) {
     vec2 xy_cent = 2. * uv - 1.;
     float angle = abs(atan(xy_cent.y, xy_cent.x));
-    float n_sides = (iIntensity * 5.) + 1.;
+    float n_sides = max((iIntensity - 0.2) * 5. + 3., 3.);
     float arc = 2. * M_PI / n_sides;
     float a1 = mod(angle, arc);
     //float lengthFactor = sqrt(2.);
@@ -11,7 +11,7 @@ void main(void) {
     float corr = cos(a1 - arc / 2.) / (lengthFactor * cos(arc / 2.));
 
     vec2 rtheta = vec2(length(xy_cent) * corr, 0.5 + angle / (2. * M_PI));
-    vec2 uv2 = mix(uv, rtheta, clamp(iIntensity * 5., 0., 1.));
+    vec2 uv2 = mix(uv, rtheta, smoothstep(0., 0.2, iIntensity));
 
-    fragColor = texture(iInput, uv2);
+    fragColor = texture(iInput, uv2) * box(uv2);
 }
