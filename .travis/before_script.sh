1e637aa7e2316299973e057e7ae2b9576d61d2a8
Eric Van Albert <eric@van.al>
2017-11-16 22:16:15 -0500

    hopefully deploy osx artifacts?

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -2,7 +2,8 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]
 then
-    cmake -DCMAKE_PREFIX_PATH=/usr/local/Cellar/qt/5.9.2/ -DCMAKE_BUILD_TYPE=Release ..
+    cmake -DCMAKE_PREFIX_PATH=/usr/local/Cellar/qt/5.9.2/ -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_CC_COMPILER=clang ..
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]
 then
     cmake -DCMAKE_PREFIX_PATH=/opt/qt59/ -DCMAKE_BUILD_TYPE=Release ..
