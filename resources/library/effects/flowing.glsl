866ef7713f48d15e740c39bfac24a3ddd088027f
Eric Van Albert <eric@van.al>
2017-11-21 20:55:23 -0500

    flowing

---
+++ resources/library/effects/flowing.glsl
@@ -0,0 +1,19 @@
+#property description Sort of like rainbow but for lightness, avoiding the edges
+
+void main(void) {
+    fragColor = texture(iInput, uv);
+    vec3 hsv = rgb2hsv(fragColor.rgb);
+
+    float deviation = mod(iTime * 0.5, 1.);
+    float spatialFrequency = 8.;
+    float newLightness = hsv.z * mod(spatialFrequency * hsv.z + deviation, 1.);
+
+    float amount = iIntensity;
+
+    // Avoid the edges to smooth the discontinuity
+    amount *= smoothstep(0., 0.4, newLightness);
+    amount *= 1. - smoothstep(0.6, 1., newLightness);
+
+    hsv.z = mix(hsv.z, newLightness, amount);
+    fragColor.rgb = hsv2rgb(hsv);
+}
