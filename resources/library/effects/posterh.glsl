745f85ca56b49092c4384b2badd1c916d97612c3
Eric Van Albert <eric@van.al>
2018-02-25 16:43:31 -0500

    convert a few more effects

--- resources/library/effects/posterh.glsl
+++ resources/library/effects/posterh.glsl
@@ -1,8 +1,9 @@
 #property description Reduce number of hues in HSV space
 
 void main(void) {
-    float bins = min(360., 6. / iIntensity);
-    
+    float parameter = iIntensity * (0.5 + 0.5 * pow(defaultPulse, 2.));
+    float bins = min(360., 6. / parameter);
+
     vec4 hsv = demultiply(texture(iInput, uv));
     hsv.rgb = rgb2hsv(hsv.rgb);
     hsv.r = mod(round(hsv.r * bins) / bins, 1.0);
