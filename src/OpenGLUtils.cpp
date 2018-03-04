fb5907a436c7989c9ac59de3236bf43d168094c7
Eric Van Albert <eric@van.al>
2018-03-04 17:02:17 -0500

    Remove unused code and typedefs from OpenGLUtils

--- src/OpenGLUtils.cpp
+++ src/OpenGLUtils.cpp
@@ -1,69 +1,10 @@
 #include "OpenGLUtils.h"
-#include <QOpenGLContext>
-#include <QOpenGLFunctions>
-#include <QOpenGLExtraFunctions>
-bool OpenGLSampler::create()
-{
-    destroy();
-    auto ctx = QOpenGLContext::currentContext();
-    if(!ctx)
-        return false;
-    auto funcs = ctx->extraFunctions();
-    if(!funcs)
-        return false;
-    funcs->glGenSamplers(1, &m_id);
-    if(!isCreated())
-        return false;
-    return true;
-}
-OpenGLSampler::~OpenGLSampler()
-{
-    destroy();
-}
-void OpenGLSampler::destroy()
-{
-    if(!isCreated())
-        return;
-    if(auto ctx = QOpenGLContext::currentContext()) {
-        if(auto fns = ctx->extraFunctions()) {
-            fns->glDeleteSamplers(1,&m_id);
-            m_id = 0u;
-        }
-    }
-}
-void OpenGLSampler::bind(GLuint unit)
-{
-    if(!isCreated())
-        return;
-    if(auto ctx = QOpenGLContext::currentContext()) {
-        if(auto fns = ctx->extraFunctions())
-            fns->glBindSampler(unit, samplerId());
-    }
-}
-void OpenGLSampler::setParameter(GLenum pname, GLint param)
-{
-    if(!isCreated())
-        return;
-    if(auto ctx = QOpenGLContext::currentContext()) {
-        if(auto fns = ctx->extraFunctions())
-            fns->glSamplerParameteri(samplerId(), pname,param);
-    }
-}
-void OpenGLSampler::setParameter(GLenum pname, GLfloat param)
-{
-    if(!isCreated())
-        return;
-    if(auto ctx = QOpenGLContext::currentContext()) {
-        if(auto fns = ctx->extraFunctions())
-            fns->glSamplerParameterf(samplerId(), pname, param);
-    }
-}
 
-QOGLShaderProgramPointer copyProgram(QOGLShaderProgramPointer program) {
+QSharedPointer<QOpenGLShaderProgram> copyProgram(QSharedPointer<QOpenGLShaderProgram> program) {
     if(!program || !program->isLinked())
         return {};
 
-    auto nprogram = QOGLShaderProgramPointer::create();
+    auto nprogram = QSharedPointer<QOpenGLShaderProgram>::create();
     for(auto shader : program->shaders())
         nprogram->addShader(shader);
 
