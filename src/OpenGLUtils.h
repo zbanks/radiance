b2a4b689e42b9779f674bfa2057d8d4ad7dd6cc1
Eric Van Albert <eric@van.al>
2018-03-04 17:02:17 -0500

    Remove unused code and typedefs from OpenGLUtils

--- src/OpenGLUtils.h
+++ src/OpenGLUtils.h
@@ -1,51 +1,6 @@
 #pragma once
 
-#include <QObject>
-#include <QOpenGLContext>
-#include <QOpenGLBuffer>
-#include <QOpenGLTexture>
 #include <QOpenGLShaderProgram>
-#include <QOpenGLFramebufferObject>
 #include <QSharedPointer>
 
-#include "OpenGLWorkerContext.h"
-
-using QOGLShaderProgramPointer = QSharedPointer<QOpenGLShaderProgram>;
-using QOGLFramebufferObjectPointer = QSharedPointer<QOpenGLFramebufferObject>;
-using QOGLTexturePointer = QSharedPointer<QOpenGLTexture>;
-using QOGLBufferPointer = QSharedPointer<QOpenGLBuffer>;
-
-class OpenGLSampler {
-    Q_GADGET
-public:
-    constexpr OpenGLSampler() = default;
-    OpenGLSampler(OpenGLSampler && o) noexcept = default;
-    OpenGLSampler &operator=(OpenGLSampler && o) noexcept = default;
-   ~OpenGLSampler();
-    operator const GLuint& () const
-    {
-        return m_id;
-    }
-    bool isCreated() const
-    {
-        return m_id != 0u;
-    }
-    bool create();
-    void destroy();
-    GLuint samplerId() const
-    {
-        return m_id;
-    };
-    void setParameter(GLenum pname, GLint param);
-    void setParameter(GLenum pname, GLfloat param);
-    void bind(GLuint where);
-protected:
-    GLuint m_id{0u};
-};
-
-struct Pass {
-    QOGLFramebufferObjectPointer m_output;
-    QOGLShaderProgramPointer     m_shader;
-};
-
-QOGLShaderProgramPointer copyProgram(QOGLShaderProgramPointer program);
+QSharedPointer<QOpenGLShaderProgram> copyProgram(QSharedPointer<QOpenGLShaderProgram> program);
