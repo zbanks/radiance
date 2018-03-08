ee1dd0080436700c9b1dd9d678a872becfbf2887
Eric Van Albert <eric@van.al>
2018-03-08 00:43:43 -0500

    Add comments about STRBON

--- src/SelfTimedReadBackOutputNode.h
+++ src/SelfTimedReadBackOutputNode.h
@@ -6,6 +6,32 @@
 #include "OpenGLWorkerContext.h"
 #include "OpenGLWorker.h"
 
+// This class is a base class for
+// writing your own simple outputs.
+
+// You should subclass this base class if:
+// 1. You just want the pixels in RAM, not on the GPU
+//    (you don't want / need OpenGL features)
+// 2. You want to process these frames in a new thread,
+//    separate from Qt's render / GUI thread
+
+// If so, this class is for you!
+
+// Subclassing this is simple:
+// 1. Connect to the "frame(QSize, QByteArray)" signal
+//    with a direct connection to receive frames
+// 2. Call start() / stop() / force() to get frames
+// 3. Implement the static functions necessary to register your
+//    output node in the registry (see registry.cpp)
+
+// A new thread will be created,
+// and frame() will be emitted in the context of this new thread.
+// initialize() is also emitted in the context of the new thread,
+// but just once at startup.
+
+// For an example of how to use this class, check out
+// ConsoleOutputNode
+
 class STRBONOpenGLWorker;
 class SelfTimedReadBackOutputNodePrivate;
 
