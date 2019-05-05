4414eaa0bea7cdca57f2b100f2d202b1abb8c03a
Eric Van Albert <eric@van.al>
2019-05-04 20:25:57 -0400

    add more .gitignore paths, tweak setup.py to reflect pypi version

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Radiance',
-    version='0.1dev',
+    version='0.1dev0',
     packages=['radiance',],
     license='MIT',
     long_description="Python tools for the Radiance video art system",
