cc269f97eb18d9ae7d6d4350b1d8ba09a17e2c23
Eric Van Albert <eric@van.al>
2019-05-04 20:22:55 -0400

    add setup.py

---
+++ python/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Radiance',
+    version='0.1dev',
+    packages=['radiance',],
+    license='MIT',
+    long_description="Python tools for the Radiance video art system",
+)
