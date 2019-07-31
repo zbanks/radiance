debee51f2eb81aea6126e32045e9d62a17c696e9
Eric Van Albert <eric@van.al>
2019-07-30 21:16:49 -0400

    Upgrade Ubuntu on Travis to Bionic

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -5,8 +5,5 @@ then
     echo "osx before_install"
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]
 then
-    sudo apt-add-repository -y ppa:ubuntu-toolchain-r/test
-    sudo apt-add-repository -y ppa:beineri/opt-qt591-trusty
-    sudo add-apt-repository -y ppa:mc3man/testing6
     sudo apt-get -qy update
 fi
--- .travis/install.sh
+++ .travis/install.sh
@@ -11,24 +11,8 @@ then
     brew install rtmidi
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]
 then
-    sudo apt-get install -qq g++-6
-    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 90
-    sudo apt-get -qy install doxygen graphviz qt59base qt59multimedia qt59quickcontrols qt59imageformats qt59quickcontrols2 qt59script libfftw3-dev libsamplerate0-dev libasound2-dev libmpv-dev
-
-    git clone --quiet --depth=100 "https://github.com/EddieRingle/portaudio" ~/builds/portaudio \
-    && pushd ~/builds/portaudio \
-    && ./configure \
-    && make \
-    && sudo make install \
-    && popd
-
-    git clone --quiet --depth=100 "https://github.com/thestk/rtmidi" ~/builds/rtmidi \
-    && pushd ~/builds/rtmidi \
-    && ./autogen.sh \
-    && ./configure \
-    && make \
-    && sudo make install \
-    && popd
+    sudo apt-get -qy install debhelper cmake qtbase5-dev qtdeclarative5-dev \
+        qtquickcontrols2-5-dev libfftw3-dev libsamplerate0-dev portaudio19-dev libmpv-dev librtmidi-dev
 
     curl -L https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage > linuxdeployqt \
     && chmod +x linuxdeployqt \
