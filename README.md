379d332f81fecab3754f271cb0a0554382285a58
Zach Banks <zjbanks@gmail.com>
2020-04-18 03:02:15 -0400

    Rendering shaders

--- README.md
+++ README.md
@@ -1,29 +1,17 @@
-[![Netlify Status](https://api.netlify.com/api/v1/badges/5ba03ba7-ff8b-4c54-94e7-cd5fd76a6737/deploy-status)](https://app.netlify.com/sites/yew-todomvc/deploys)
-
 ## About
 
 This template shows how to create a web app using Yew and wasm-pack. 
 
-## 🚴 Usage
+## Usage
 
-### 🛠️ Build with `yarn run build`
+### Build with `yarn run build`
 
 ```
 yarn run build
 ```
 
-### 🔬 Serve locally with `yarn run start:dev`
+### Serve locally with `yarn run start:dev`
 
 ```
 yarn run start:dev
 ```
-
-
-## 🔋 Batteries Included
-
-* [`wasm-bindgen`](https://github.com/rustwasm/wasm-bindgen) for communicating
-  between WebAssembly and JavaScript.
-* [`console_error_panic_hook`](https://github.com/rustwasm/console_error_panic_hook)
-  for logging panic messages to the developer console.
-* [`wee_alloc`](https://github.com/rustwasm/wee_alloc), an allocator optimized
-  for small code size.
--- src/app.rs
+++ src/app.rs
@@ -4,23 +4,42 @@ use log::*;
 //use strum_macros::{EnumIter, ToString};
 //use yew::format::Json;
 //use yew::services::storage::{Area, StorageService};
-use yew::services::{RenderService,Task};
-use yew::prelude::*;
 use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+use yew::services::{RenderService, Task};
 
 use wasm_bindgen::JsCast;
-use web_sys::{WebGlProgram, WebGlRenderingContext, WebGlShader};
+use web_sys::WebGlRenderingContext as GL;
+use web_sys::{WebGlFramebuffer, WebGlProgram, WebGlRenderingContext, WebGlShader, WebGlTexture, WebGlBuffer};
+use std::rc::Rc;
 
 pub struct App {
     link: ComponentLink<Self>,
-    model: Model,
+    model: Option<Model>,
     node_ref: NodeRef,
-    gl_context: Option<WebGlRenderingContext>,
     render_loop: Option<Box<dyn Task>>,
 }
 
-pub struct Model {
+pub struct Fbo {
+    context: Rc<WebGlRenderingContext>,
+    texture: WebGlTexture,
+    framebuffer: WebGlFramebuffer,
+}
 
+pub struct ShaderNode {
+    context: Rc<WebGlRenderingContext>,
+    program: WebGlProgram,
+    frag_shader: WebGlShader,
+    vert_shader: WebGlShader,
+    fbo: Fbo,
+}
+
+pub struct Model {
+    context: Rc<WebGlRenderingContext>,
+    shaders: Vec<ShaderNode>,
+    blit_shader: ShaderNode,
+    extra_fbo: Fbo,
+    vertex_buffer: WebGlBuffer,
 }
 
 pub enum Msg {
@@ -32,14 +51,23 @@ impl Component for App {
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        let model = Model {};
-        App { link, model, node_ref: Default::default(), gl_context: None, render_loop: None }
+        App {
+            link,
+            model: None,
+            node_ref: Default::default(),
+            render_loop: None,
+        }
     }
 
     fn mounted(&mut self) -> ShouldRender {
         let canvas: web_sys::HtmlCanvasElement = self.node_ref.cast().unwrap();
-        let gl_context = canvas.get_context("webgl").expect("WebGL not supported").unwrap().dyn_into::<WebGlRenderingContext>().unwrap();
-        self.gl_context = Some(gl_context);
+        let gl_context = canvas
+            .get_context("webgl")
+            .expect("WebGL not supported")
+            .unwrap()
+            .dyn_into::<WebGlRenderingContext>()
+            .unwrap();
+        self.model = Some(Model::new(gl_context));
 
         self.schedule_next_render();
 
@@ -65,66 +93,64 @@ impl Component for App {
     }
 }
 
-
 impl App {
     fn render_gl(&mut self, time: f64) -> () {
-        let gl_context = self.gl_context.as_ref().unwrap();
-        let vert_shader = compile_shader(
-            gl_context,
-            WebGlRenderingContext::VERTEX_SHADER,
-            r#"
-            attribute vec4 position;
-            void main() {
-                gl_Position = position;
-            }
-        "#,
-        ).unwrap();
-        let frag_shader = compile_shader(
-            gl_context,
-            WebGlRenderingContext::FRAGMENT_SHADER,
-            r#"
-            void main() {
-                gl_FragColor = vec4(0.0, 1.0, 1.0, 1.0);
-            }
-        "#,
-        ).unwrap();
-        let program = link_program(&gl_context, &vert_shader, &frag_shader).unwrap();
-        gl_context.use_program(Some(&program));
+        let model = self.model.as_mut().unwrap();
+        let gl_context = &model.context;
 
-        let vertices: [f32; 9] = [-0.7, f32::sin(time as f32 / 1000.), 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
+        gl_context.disable(GL::DEPTH_TEST);
+        gl_context.disable(GL::BLEND);
+        gl_context.clear_color(0.0, 0.0, 0.0, 0.0);
+        gl_context.clear(GL::COLOR_BUFFER_BIT);
 
-        let buffer = gl_context.create_buffer().ok_or("failed to create buffer").unwrap();
-        gl_context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
+        for shader in &mut model.shaders {
+            //gl_context.bind_framebuffer(GL::FRAMEBUFFER, None);
+            //model.extra_fbo.bind();
+            gl_context.bind_framebuffer(GL::FRAMEBUFFER, Some(&model.extra_fbo.framebuffer));
+            gl_context.active_texture(GL::TEXTURE0);
+            gl_context.bind_texture(GL::TEXTURE_2D, None);
 
-        // Note that `Float32Array::view` is somewhat dangerous (hence the
-        // `unsafe`!). This is creating a raw view into our module's
-        // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
-        // (aka do a memory allocation in Rust) it'll cause the buffer to change,
-        // causing the `Float32Array` to be invalid.
-        //
-        // As a result, after `Float32Array::view` we have to be very careful not to
-        // do any memory allocations before it's dropped.
-        unsafe {
-            let vert_array = js_sys::Float32Array::view(&vertices);
+            gl_context.use_program(Some(&shader.program));
 
-            gl_context.buffer_data_with_array_buffer_view(
-                WebGlRenderingContext::ARRAY_BUFFER,
-                &vert_array,
-                WebGlRenderingContext::STATIC_DRAW,
+            let position_loc = gl_context.get_attrib_location(&shader.program, "vPosition");
+
+            gl_context.bind_buffer(GL::ARRAY_BUFFER, Some(&model.vertex_buffer));
+            gl_context.enable_vertex_attrib_array(position_loc as u32);
+            gl_context.vertex_attrib_pointer_with_i32(position_loc as u32, 4, GL::FLOAT, false, 0, 0);
+
+            gl_context.draw_arrays(
+                GL::TRIANGLE_STRIP,
+                0,
+                4
             );
+            gl_context.active_texture(GL::TEXTURE0);
+            gl_context.use_program(None);
+            std::mem::swap(&mut model.extra_fbo, &mut shader.fbo);
         }
 
-        gl_context.vertex_attrib_pointer_with_i32(0, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
-        gl_context.enable_vertex_attrib_array(0);
+        {
+            gl_context.bind_framebuffer(GL::FRAMEBUFFER, None);
+            gl_context.active_texture(GL::TEXTURE0);
+            gl_context.bind_texture(GL::TEXTURE_2D, Some(&model.shaders.last().unwrap().fbo.texture));
 
-        gl_context.clear_color(0.0, 0.0, 0.0, 1.0);
-        gl_context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+            gl_context.use_program(Some(&model.blit_shader.program));
 
-        gl_context.draw_arrays(
-            WebGlRenderingContext::TRIANGLES,
-            0,
-            (vertices.len() / 3) as i32,
-        );
+            let position_loc = gl_context.get_attrib_location(&model.blit_shader.program, "vPosition");
+            gl_context.bind_buffer(GL::ARRAY_BUFFER, Some(&model.vertex_buffer));
+            gl_context.enable_vertex_attrib_array(position_loc as u32);
+            gl_context.vertex_attrib_pointer_with_i32(position_loc as u32, 4, GL::FLOAT, false, 0, 0);
+
+            let texture_loc = gl_context.get_uniform_location(&model.blit_shader.program, "uSampler");
+            gl_context.uniform1i(texture_loc.as_ref(), 0);
+
+            gl_context.draw_arrays(
+                GL::TRIANGLE_STRIP,
+                0,
+                4
+            );
+            gl_context.active_texture(GL::TEXTURE0);
+            gl_context.use_program(None);
+        }
 
         self.schedule_next_render();
     }
@@ -138,56 +164,203 @@ impl App {
     }
 }
 
-pub fn compile_shader(
-    context: &WebGlRenderingContext,
-    shader_type: u32,
-    source: &str,
-) -> Result<WebGlShader, String> {
-    let shader = context
-        .create_shader(shader_type)
-        .ok_or_else(|| String::from("Unable to create shader object"))?;
-    context.shader_source(&shader, source);
-    context.compile_shader(&shader);
+impl ShaderNode {
+    fn new(context: Rc<WebGlRenderingContext>) -> ShaderNode {
+        Self::from_fragment_shader(context, 
+            r#"
 
-    if context
-        .get_shader_parameter(&shader, WebGlRenderingContext::COMPILE_STATUS)
-        .as_bool()
-        .unwrap_or(false)
-    {
-        Ok(shader)
-    } else {
-        Err(context
-            .get_shader_info_log(&shader)
-            .unwrap_or_else(|| String::from("Unknown error creating shader")))
+            precision mediump float;
+            varying highp vec2 uv;
+
+            void main() {
+                gl_FragColor = vec4(0.5, uv.y, 1.0, 1.0);
+            }
+        "#)
+    }
+
+    fn from_fragment_shader(context: Rc<WebGlRenderingContext>, shader_code: &str) -> ShaderNode {
+        info!("Compiling shaders");
+        let vert_shader = Self::compile_shader(
+            &context,
+            WebGlRenderingContext::VERTEX_SHADER,
+            r#"
+            attribute vec4 vPosition;
+            varying highp vec2 uv;
+
+            void main() {
+                gl_Position = vec4(vPosition.xy, 0., 1.);
+                uv = vPosition.zw;
+            }
+        "#,
+        )
+        .unwrap();
+        let frag_shader = Self::compile_shader(
+            &context,
+            WebGlRenderingContext::FRAGMENT_SHADER,
+            shader_code,
+        )
+        .unwrap();
+        let program = Self::link_program(&context, &vert_shader, &frag_shader).unwrap();
+        let fbo = Fbo::new(Rc::clone(&context));
+
+        ShaderNode { context, program, frag_shader, vert_shader, fbo }
+    }
+
+    fn compile_shader(
+        context: &WebGlRenderingContext,
+        shader_type: u32,
+        source: &str,
+    ) -> Result<WebGlShader, String> {
+        let shader = context
+            .create_shader(shader_type)
+            .ok_or_else(|| String::from("Unable to create shader object"))?;
+        context.shader_source(&shader, source);
+        context.compile_shader(&shader);
+
+        if context
+            .get_shader_parameter(&shader, WebGlRenderingContext::COMPILE_STATUS)
+            .as_bool()
+            .unwrap_or(false)
+        {
+            Ok(shader)
+        } else {
+            Err(context
+                .get_shader_info_log(&shader)
+                .unwrap_or_else(|| String::from("Unknown error creating shader")))
+        }
+    }
+
+    fn link_program(
+        context: &WebGlRenderingContext,
+        vert_shader: &WebGlShader,
+        frag_shader: &WebGlShader,
+    ) -> Result<WebGlProgram, String> {
+        let program = context
+            .create_program()
+            .ok_or_else(|| String::from("Unable to create shader object"))?;
+
+        context.attach_shader(&program, vert_shader);
+        context.attach_shader(&program, frag_shader);
+        context.link_program(&program);
+
+        if context
+            .get_program_parameter(&program, WebGlRenderingContext::LINK_STATUS)
+            .as_bool()
+            .unwrap_or(false)
+        {
+            Ok(program)
+        } else {
+            Err(context
+                .get_program_info_log(&program)
+                .unwrap_or_else(|| String::from("Unknown error creating program object")))
+        }
     }
 }
 
-pub fn link_program(
-    context: &WebGlRenderingContext,
-    vert_shader: &WebGlShader,
-    frag_shader: &WebGlShader,
-) -> Result<WebGlProgram, String> {
-    let program = context
-        .create_program()
-        .ok_or_else(|| String::from("Unable to create shader object"))?;
+impl Drop for ShaderNode {
+    fn drop(&mut self) -> () {
+        self.context.delete_shader(Some(&self.frag_shader));
+        self.context.delete_shader(Some(&self.vert_shader));
+        self.context.delete_program(Some(&self.program));
+    }
+}
 
-    context.attach_shader(&program, vert_shader);
-    context.attach_shader(&program, frag_shader);
-    context.link_program(&program);
+impl Fbo {
+    fn new(context: Rc<WebGlRenderingContext>) -> Fbo {
+        let texture = context.create_texture().unwrap();
+        context.bind_texture(GL::TEXTURE_2D, Some(&texture));
+        context.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
+            GL::TEXTURE_2D,                  // target
+            0,                               // level
+            GL::RGBA as i32,                 // internal format
+            context.drawing_buffer_width(),  // width
+            context.drawing_buffer_height(), // height
+            0,                               // border
+            GL::RGBA,                        // format
+            GL::UNSIGNED_BYTE,               // _type
+            None,                            // pixels
+        );
+        context.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER, GL::LINEAR as i32);
+        context.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER, GL::LINEAR as i32);
+        context.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_WRAP_S, GL::CLAMP_TO_EDGE as i32);
+        context.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_WRAP_T, GL::CLAMP_TO_EDGE as i32);
 
-    if context
-        .get_program_parameter(&program, WebGlRenderingContext::LINK_STATUS)
-        .as_bool()
-        .unwrap_or(false)
-    {
-        Ok(program)
-    } else {
-        Err(context
-            .get_program_info_log(&program)
-            .unwrap_or_else(|| String::from("Unknown error creating program object")))
+        let framebuffer = context.create_framebuffer().unwrap();
+        context.bind_framebuffer(GL::FRAMEBUFFER, Some(&framebuffer));
+        context.framebuffer_texture_2d(
+            GL::FRAMEBUFFER,
+            GL::COLOR_ATTACHMENT0,
+            GL::TEXTURE_2D,
+            Some(&texture),
+            0, /* level */
+        );
+
+        Fbo {
+            context,
+            texture,
+            framebuffer,
+        }
+    }
+
+    fn bind(&self) -> () {
+        self.context.bind_framebuffer(GL::FRAMEBUFFER, Some(&self.framebuffer));
+    }
+}
+
+impl Drop for Fbo {
+    fn drop(&mut self) -> () {
+        self.context.delete_texture(Some(&self.texture));
+        self.context.delete_framebuffer(Some(&self.framebuffer));
     }
 }
 
 impl Model {
+    fn new(context: WebGlRenderingContext) -> Model {
+        info!("New model & context!");
+        let context = Rc::new(context);
+        let shaders = vec![ShaderNode::new(Rc::clone(&context))];
+        let extra_fbo = Fbo::new(Rc::clone(&context));
 
+        let blit_shader = ShaderNode::from_fragment_shader(Rc::clone(&context), r#"
+            varying highp vec2 uv;
+            uniform sampler2D uSampler;
+            void main() {
+                gl_FragColor = texture2D(uSampler, uv);
+            }
+        "#);
+
+        let vertices: [f32; 16] = [
+             1.0,  1.0, 1.0, 1.0,
+            -1.0,  1.0, 0.0, 1.0,
+             1.0, -1.0, 1.0, 0.0,
+            -1.0, -1.0, 0.0, 0.0,
+        ];
+
+        let vertex_buffer = context .create_buffer() .ok_or("failed to create buffer") .unwrap();
+        context.bind_buffer(GL::ARRAY_BUFFER, Some(&vertex_buffer));
+
+        // Note: there is an alternate way to do this with Float32Array::view
+        // that is "faster" and avoids copying this buffer; but this buffer is so
+        // small I don't think it's worth the use of `unsafe`
+        let vert_array = js_sys::Float32Array::from(&vertices as &[f32]);
+        context.buffer_data_with_opt_array_buffer(
+            GL::ARRAY_BUFFER,
+            Some(&vert_array.buffer()),
+            GL::STATIC_DRAW,
+        );
+
+        Model {
+            context,
+            shaders,
+            extra_fbo,
+            vertex_buffer,
+            blit_shader,
+        }
+    }
+}
+
+impl Drop for Model {
+    fn drop(&mut self) -> () {
+        self.context.delete_buffer(Some(&self.vertex_buffer));
+    }
 }
