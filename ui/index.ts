140d06c846b7f02211b3e236b76d6ed7ef8b2d7e
Eric Van Albert <eric@van.al>
2020-04-26 13:18:06 -0400

    Work on flickable surface

--- ui/index.ts
+++ ui/index.ts
@@ -1,13 +1,171 @@
 "use strict";
 
+class Flickable extends HTMLElement {
+    outer: HTMLElement; // Outer div (for clipping and mouse events)
+    inner: HTMLElement; // Inner div (the one that is transformed)
+    offsetX: number; // Current X offset of the surface
+    offsetY: number; // Current Y offset of the surface
+    dragging: boolean; // Whether or not the surface is being dragged
+    mouseDrag: boolean; // Whether or not the surface is being dragged due to the mouse
+    touchDrag: boolean; // Whether or not the surface is being dragged due to a touch
+    startDragX: number; // X coordinate of the start of the drag
+    startDragY: number; // Y coordinate of the start of the drag
+    startDragOffsetX: number; // The value of offsetX when the drag starts
+    startDragOffsetY: number; // The value of offsetY when the drag starts
+    touchId; // The identifier of the touchevent causing the drag
+    resizeObserver: ResizeObserver;
+
+    constructor() {
+        super();
+        const shadow = this.attachShadow({mode: 'open'});
+        shadow.innerHTML = `
+            <style>
+            :host {
+                display: block;
+                width: 640px;
+                height: 480px;
+            }
+            #outer {
+                display: flex;
+                width: 100%;
+                height: 100%;
+                justify-content:center;
+                align-items:center;
+                overflow: hidden;
+            }
+            #inner {
+                padding: 30px;
+            }
+            </style> 
+            <div id='outer'>
+                <div id='inner'>
+                    <slot></slot>
+                </div>
+            </div>
+        `;
+        this.inner = shadow.querySelector("#inner");
+        this.outer = shadow.querySelector("#outer");
+        this.outer.addEventListener('mousedown', this.mouseDown.bind(this));
+        document.addEventListener('mouseup', this.mouseUp.bind(this));
+        document.addEventListener('mousemove', this.mouseMove.bind(this));
+        this.outer.addEventListener('touchstart', this.touchStart.bind(this), { passive: false });
+        document.addEventListener('touchend', this.touchEnd.bind(this));
+        document.addEventListener('touchmove', this.touchMove.bind(this));
+
+        this.resizeObserver = new ResizeObserver(entries => {
+            this.applyTransformation();
+        });
+        this.resizeObserver.observe(this.outer);
+        this.resizeObserver.observe(this.inner);
+
+        this.offsetX = 0;
+        this.offsetY = 0;
+    }
+
+    applyTransformation() {
+        let rangeX = 0.5 * Math.max(0, this.inner.offsetWidth - this.outer.clientWidth);
+        let rangeY = 0.5 * Math.max(0, this.inner.offsetHeight - this.outer.clientHeight);
+        if (Math.abs(this.offsetX) > rangeX) {
+            this.offsetX = rangeX * Math.sign(this.offsetX);
+        }
+        if (Math.abs(this.offsetY) > rangeY) {
+            this.offsetY = rangeY * Math.sign(this.offsetY);
+        }
+        this.inner.style.transform = `translate(${this.offsetX}px, ${this.offsetY}px)`;
+    }
+
+    touchStart(event: TouchEvent) {
+        for (let i = 0; i < event.changedTouches.length; i++) {
+            let touch = event.changedTouches.item(i);
+            if (touch.target != this.outer && touch.target != this.inner) {
+                // Only accept clicks onto this surface, not its child (inner)
+                continue;
+            }
+            if (!this.dragging) {
+                this.touchDrag = true;
+                this.touchId = touch.identifier
+                this.startDrag(touch.pageX, touch.pageY);
+                event.preventDefault();
+            }
+        }
+    }
+
+    mouseDown(event: MouseEvent) {
+        if (event.target != this.outer && event.target != this.inner) {
+            // Only accept clicks onto this surface, not its child (inner)
+            return;
+        }
+        if (!this.dragging && (event.buttons & 1)) {
+            this.mouseDrag = true;
+            this.startDrag(event.pageX, event.pageY);
+            event.preventDefault();
+        }
+    }
+
+    touchEnd(event: TouchEvent) {
+        for (let i = 0; i < event.changedTouches.length; i++) {
+            let touch = event.changedTouches.item(i);
+            if (this.dragging && this.touchDrag && this.touchId == touch.identifier) {
+                this.touchDrag = false;
+                this.endDrag();
+            }
+        }
+    }
+
+    mouseUp(event: MouseEvent) {
+        if (this.dragging && this.mouseDrag && !(event.buttons & 1)) {
+            this.mouseDrag = false;
+            this.endDrag();
+            event.preventDefault();
+        }
+    }
+
+    touchMove(event: TouchEvent) {
+        for (let i = 0; i < event.changedTouches.length; i++) {
+            let touch = event.changedTouches.item(i);
+            if (this.dragging && this.touchDrag && this.touchId == touch.identifier) {
+                this.drag(touch.pageX, touch.pageY);
+            }
+        }
+    }
+
+    mouseMove(event: MouseEvent) {
+        if (this.dragging && this.mouseDrag) {
+            this.drag(event.pageX, event.pageY);
+            event.preventDefault();
+        }
+    }
+
+    startDrag(ptX: number, ptY: number) {
+        this.dragging = true;
+        this.startDragX = ptX;
+        this.startDragY = ptY;
+        this.startDragOffsetX = this.offsetX;
+        this.startDragOffsetY = this.offsetY;
+    }
+
+    endDrag() {
+        this.dragging = false;
+    }
+
+    drag(ptX: number, ptY: number) {
+        this.offsetX = this.startDragOffsetX + ptX - this.startDragX;
+        this.offsetY = this.startDragOffsetY + ptY - this.startDragY;
+        this.applyTransformation();
+    }
+}
+
 class Graph extends HTMLElement {
+    outer: HTMLElement;
+    inner: HTMLElement;
+
     constructor() {
         super();
         let shadow = this.attachShadow({mode: 'open'});
-        let content = document.createElement('div');
-        content.textContent = 'Hello, radiance';
-        shadow.appendChild(content);
+        shadow.innerHTML = `
+        `;
     }
 }
 
 customElements.define('radiance-graph', Graph);
+customElements.define('radiance-flickable', Flickable);
---
+++ ui/typing.d.ts
@@ -0,0 +1,26 @@
+// https://stackoverflow.com/a/59684145
+
+declare interface ResizeObserver {
+  observe(target: Element): void;
+  unobserve(target: Element): void;
+  disconnect(): void;
+}
+
+declare var ResizeObserver: {
+  prototype: ResizeObserver;
+  new(callback: ResizeObserverCallback): ResizeObserver;
+};
+
+interface ResizeObserverSize {
+    inlineSize: number;
+    blockSize: number;
+}
+
+type ResizeObserverCallback = (entries: ReadonlyArray<ResizeObserverEntry>, observer: ResizeObserver) => void;
+
+interface ResizeObserverEntry {
+    readonly target: Element;
+    readonly contentRect: DOMRectReadOnly;
+    readonly borderBoxSize: ResizeObserverSize;
+    readonly contentBoxSize: ResizeObserverSize;
+}
