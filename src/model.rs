e74284bdc61f8095132d363c4b68cb3166168d7c
Zach Banks <zjbanks@gmail.com>
2020-05-02 00:32:40 -0400

    Create Graph struct that can actually represent the Radiance graph

--- src/model.rs
+++ src/model.rs
@@ -7,7 +7,166 @@ use petgraph::graphmap::DiGraphMap;
 use serde::{Deserialize, Serialize};
 use serde_json::Value as JsonValue;
 use std::borrow::{Borrow, BorrowMut};
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap, BTreeSet};
+
+struct Graph<T: Copy + Eq + Ord + std::fmt::Debug> {
+    /// Map of to_vertex -> [from_vertex_0, from_vertex_1, ...]
+    edges: BTreeMap<T, Vec<Option<T>>>,
+}
+
+#[allow(dead_code,unused_variables)]
+impl<T: Copy + Eq + Ord + std::fmt::Debug> Graph<T> {
+    pub fn new() -> Graph<T> {
+        Graph {
+            edges: Default::default(),
+        }
+    }
+
+    /// Completely remove all graph state
+    pub fn clear(&mut self) {
+        self.edges.clear();
+    }
+
+    /// Add, modify, or remove a vertex
+    /// If `n_inputs` is `None`, remove the vertex and any associated edges
+    pub fn set_vertex(&mut self, vertex: T, n_inputs: Option<usize>) {
+        if let Some(n_inputs) = n_inputs {
+            self.edges.entry(vertex).or_default().resize(n_inputs, None);
+        } else {
+            self.edges.remove(&vertex);
+
+            // Remove all edges where `vertex` was the `from_vertex`
+            for from_vertexes in self.edges.values_mut() {
+                for from_vertex in from_vertexes.iter_mut() {
+                    if *from_vertex == Some(vertex) {
+                        *from_vertex = None
+                    }
+                }
+            }
+        }
+    }
+
+    /// Add, modify, or remove an edge
+    /// If `to_input` is `None`, remove the edge
+    pub fn set_edge(&mut self, from_vertex: Option<T>, to_vertex: T, to_input: usize) -> Result<()> {
+        // Check that both verticies in the proposed edge exist
+        if let Some(fv) = from_vertex {
+            if !self.edges.contains_key(&fv) {
+                return Err(format!("from_vertex {:?} not in graph", fv).as_str().into());
+            }
+        }
+        if !self.edges.contains_key(&to_vertex) {
+            return Err(format!("to_vertex {:?} not in graph", to_vertex).as_str().into());
+        }
+
+        // TODO validation that this won't create a cycle
+
+        // Remove the previous edge that went to `(to_vertex, to_input)` & insert from_vertex
+        let inputs = self.edges.get_mut(&to_vertex).unwrap();
+        *inputs.get_mut(to_input).ok_or("invalid to_input; too large")? = from_vertex;
+
+        Ok(())
+    }
+
+    /// Return a Vec of verticies in topological order
+    pub fn toposort(&self) -> Result<Vec<T>> {
+        let mut rev_edges: BTreeMap<T, BTreeSet<T>> = self.edges.keys().map(|v| (*v, Default::default())).collect();
+        for (to_vertex, inputs) in &self.edges {
+            for from_vertex in inputs {
+                if let Some(from_vertex) = from_vertex {
+                    rev_edges.get_mut(from_vertex).unwrap().insert(*to_vertex);
+                }
+            }
+        }
+        println!("_edges: {:?}", self.edges);
+        println!("rev_edges: {:?}", rev_edges);
+
+        // Kahn's Algorithm from Wikipedia's pseudocode
+        // https://en.wikipedia.org/wiki/Topological_sorting#Kahn's_algorithm
+        // NB: The direction is inverted from the pseudocode description
+        //
+        // L ← Empty list that will contain the sorted elements
+        let mut l: Vec<T> = Default::default();
+        // S ← Set of all nodes with no incoming edge
+        let mut s: Vec<T> = rev_edges.iter().filter_map(|(v, outs)| if outs.is_empty() { Some(*v) } else { None }).collect();
+
+        // while S is non-empty do
+        //     remove a node n from S
+        while let Some(n) = s.pop() {
+            // add n to tail of L
+            l.push(n);
+            // for each node m with an edge e from n to m do
+            for m in self.edges.get(&n).unwrap().iter().filter_map(|x| x.as_ref()) {
+                // remove edge e from the graph
+                let m_edges = rev_edges.get_mut(m).unwrap();
+                m_edges.remove(&n);
+                // if m has no other incoming edges then
+                if m_edges.is_empty() {
+                    // insert m into S
+                    s.push(*m);
+                }
+            }
+        }
+        // if graph has edges then
+        if !rev_edges.values().all(|outs| outs.is_empty()) {
+            // return error   (graph has at least one cycle)
+            println!("{:?}", rev_edges);
+            //Err("Cycle detected".into())
+            Ok(l)
+        // else 
+        } else {
+            // return L   (a topologically sorted order)
+            Ok(l)
+        }
+    }
+
+    /// Return a Vec of input edges for a given vertex
+    pub fn vertex_inputs(&self, vertex: T) -> Result<&Vec<Option<T>>> {
+        self.edges.get(&vertex).ok_or_else(|| format!("Invalid vertex {:?}", vertex).as_str().into())
+    }
+
+    /// Check for cycles or other malformed representations
+    /// This is intended to be a debugging tool; Graph should never intentionally
+    /// hold malformed or cyclic graphs.
+    fn validate(&self) -> Result<()> {
+        // TODO
+        self.toposort().map(|_| ())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn graph_toposort() {
+        let mut g: Graph<usize> = Graph::new();
+        g.set_vertex(0, Some(2));
+        g.set_vertex(1, Some(2));
+        g.set_vertex(2, Some(2));
+        g.set_vertex(3, Some(2));
+        g.set_vertex(4, Some(2));
+        assert_eq!(g.toposort().unwrap(), vec![4, 3, 2, 1, 0]);
+
+        g.set_edge(Some(0), 1, 0).unwrap();
+        g.set_edge(Some(1), 2, 0).unwrap();
+        g.set_edge(Some(2), 4, 0).unwrap();
+        g.set_edge(Some(4), 3, 0).unwrap();
+        assert_eq!(g.toposort().unwrap(), vec![3, 4, 2, 1, 0]);
+
+        g.set_edge(Some(0), 3, 0).unwrap();
+        assert_eq!(g.toposort().unwrap(), vec![4, 2, 1, 3, 0]);
+
+        g.set_edge(Some(3), 4, 0).unwrap();
+        g.set_edge(Some(1), 4, 1).unwrap();
+        assert_eq!(g.vertex_inputs(0).unwrap(), &vec![None, None]);
+        assert_eq!(g.vertex_inputs(1).unwrap(), &vec![Some(0), None]);
+        assert_eq!(g.vertex_inputs(2).unwrap(), &vec![Some(1), None]);
+        assert_eq!(g.vertex_inputs(3).unwrap(), &vec![Some(0), None]);
+        assert_eq!(g.vertex_inputs(4).unwrap(), &vec![Some(3), Some(1)]);
+        assert_eq!(g.toposort().unwrap(), vec![4, 3, 2, 1, 0]);
+    }
+}
 
 /// Directed graph abstraction that owns VideoNodes
 /// - Enforces that there are no cycles
