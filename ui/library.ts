64c8b11ea29821f2aa94d15dc70f5fcbe40925e2
Eric Van Albert <eric@van.al>
2020-05-10 16:41:17 -0400

    Allow adding nodes through library

---
+++ ui/library.ts
@@ -0,0 +1,52 @@
+import {Graph} from "./graph";
+
+export class Library extends HTMLElement {
+    nodeNameInput: HTMLInputElement;
+    graph: Graph;
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const shadow = this.attachShadow({mode: 'open'});
+        shadow.innerHTML = `
+            <style>
+            :host {
+                display: flex;
+                padding: 10px;
+                flex-direction: column;
+                background-color: #333333CC;
+            }
+
+            #nodeName {
+                margin-top: auto;
+                width: 100%;
+            }
+            </style>
+            <input type="text" id="nodeName"></input>
+        `;
+        this.nodeNameInput = shadow.querySelector("#nodeName");
+        document.addEventListener('keypress', this.onKeyPress.bind(this));
+        this.nodeNameInput.addEventListener('keypress', this.onTextBoxKeyPress.bind(this));
+    }
+
+    onKeyPress(event: KeyboardEvent) {
+        if (event.code == "Semicolon" && event.shiftKey && event.target != this) {
+            this.nodeNameInput.focus();
+            event.preventDefault();
+        }
+    }
+
+    onTextBoxKeyPress(event: KeyboardEvent) {
+        if (event.code == "Enter") {
+            let nodeName = this.nodeNameInput.value.trim();
+            if (nodeName != "" && this.graph !== undefined) {
+                this.graph.addNode({nodeType: "EffectNode", name: nodeName});
+            }
+            this.nodeNameInput.value = "";
+        }
+    }
+}
+
+customElements.define('radiance-library', Library);
