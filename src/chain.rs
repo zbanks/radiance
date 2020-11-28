95a6ed5fef5af380d65c8680a3014eebe10bd6ea
Eric Van Albert <eric@van.al>
2020-11-27 23:56:46 -0500

    Texture generation

--- src/chain.rs
+++ src/chain.rs
@@ -1,14 +1,69 @@
 use crate::context::GraphicsContext;
+use crate::context::Texture;
+use rand;
 
 pub struct Chain {
     size: (usize, usize),
+    noise_texture: Texture,
 }
 
 impl Chain {
     /// Construct a new chain for a given texture size
-    pub fn new(graphics_context: &mut GraphicsContext, size: (usize, usize)) -> Result<Chain, ()> { 
-        Ok(Chain {
+    pub fn new(graphics_context: &GraphicsContext, size: (usize, usize)) -> Chain {
+        let texture_size = wgpu::Extent3d {
+            width: size.0 as u32,
+            height: size.1 as u32,
+            depth: 1,
+        };
+        let texture = graphics_context.device.create_texture(
+            &wgpu::TextureDescriptor {
+                size: texture_size,
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
+                label: Some("blank_texture"),
+            }
+        );
+
+        let random_bytes: Vec<u8> = (0 .. size.0 * size.1 * 4).map(|_| { rand::random::<u8>() }).collect();
+
+        graphics_context.queue.write_texture(
+            wgpu::TextureCopyView {
+                texture: &texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            &random_bytes,
+            wgpu::TextureDataLayout {
+                offset: 0,
+                bytes_per_row: (4 * size.0) as u32,
+                rows_per_image: size.1 as u32,
+            },
+            texture_size,
+        );
+
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let sampler = graphics_context.device.create_sampler(
+            &wgpu::SamplerDescriptor {
+                address_mode_u: wgpu::AddressMode::ClampToEdge,
+                address_mode_v: wgpu::AddressMode::ClampToEdge,
+                address_mode_w: wgpu::AddressMode::ClampToEdge,
+                mag_filter: wgpu::FilterMode::Linear,
+                min_filter: wgpu::FilterMode::Linear,
+                mipmap_filter: wgpu::FilterMode::Nearest,
+                ..Default::default()
+            }
+        );
+
+        Chain {
             size: size,
-        })
+            noise_texture: Texture {
+                texture: texture,
+                view: view,
+                sampler: sampler,
+            }
+        }
     }
 }
--- src/context.rs
+++ src/context.rs
@@ -1,23 +1,82 @@
 use crate::chain::Chain;
 use wgpu;
 
+pub struct Texture {
+    pub texture: wgpu::Texture,
+    pub view: wgpu::TextureView,
+    pub sampler: wgpu::Sampler,
+}
+
 pub struct GraphicsContext<'a> {
-    device: &'a wgpu::Device,
-    queue: &'a wgpu::Queue,
+    pub device: &'a wgpu::Device,
+    pub queue: &'a wgpu::Queue,
 }
 
 pub struct Context<'a> {
     chains: Vec<Chain>,
     graphics_context: GraphicsContext<'a>,
+    blank_texture: Texture,
 }
 
 impl<'a> Context<'a> {
     pub fn new(device: &'a wgpu::Device, queue: &'a wgpu::Queue) -> Context<'a> {
+
+        // Create blank texture
+        let texture_size = wgpu::Extent3d {
+            width: 1,
+            height: 1,
+            depth: 1,
+        };
+        let texture = device.create_texture(
+            &wgpu::TextureDescriptor {
+                size: texture_size,
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                usage: wgpu::TextureUsage::SAMPLED | wgpu::TextureUsage::COPY_DST,
+                label: Some("blank_texture"),
+            }
+        );
+
+        queue.write_texture(
+            wgpu::TextureCopyView {
+                texture: &texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            &[0, 0, 0, 0],
+            wgpu::TextureDataLayout {
+                offset: 0,
+                bytes_per_row: 4,
+                rows_per_image: 1,
+            },
+            texture_size,
+        );
+
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let sampler = device.create_sampler(
+            &wgpu::SamplerDescriptor {
+                address_mode_u: wgpu::AddressMode::ClampToEdge,
+                address_mode_v: wgpu::AddressMode::ClampToEdge,
+                address_mode_w: wgpu::AddressMode::ClampToEdge,
+                mag_filter: wgpu::FilterMode::Nearest,
+                min_filter: wgpu::FilterMode::Nearest,
+                mipmap_filter: wgpu::FilterMode::Nearest,
+                ..Default::default()
+            }
+        );
+
         Context {
             chains: Vec::new(),
             graphics_context: GraphicsContext {
                 device: device,
                 queue: queue,
+            },
+            blank_texture: Texture {
+                texture: texture,
+                view: view,
+                sampler: sampler,
             }
         }
     }
