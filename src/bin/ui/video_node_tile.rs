e140c6f806b7a81b193b8117c50e842cb7377a08
Eric Van Albert <eric@van.al>
2022-10-11 20:58:15 -0400

    Switch to pos2 from nalgebra

--- src/bin/ui/video_node_tile.rs
+++ src/bin/ui/video_node_tile.rs
@@ -1,6 +1,5 @@
 use egui::{Id, Vec2, Ui, vec2, Sense, Layout, Align, InnerResponse, Color32, Stroke, Rect, Response, pos2, TextureId, Mesh, Pos2, Shape};
 use std::hash::Hash;
-use na::Vector2;
 
 pub struct VideoNodeTile<'a> {
     id: Id,
@@ -10,8 +9,7 @@ pub struct VideoNodeTile<'a> {
     outputs: &'a [f32],
 }
 
-// WTF nalgebra, where is your 2D cross product
-fn cross(a: &Vector2<f32>, b: &Vector2<f32>) -> f32 {
+fn cross(a: Vec2, b: Vec2) -> f32 {
     a.x * b.y - a.y * b.x
 }
 
@@ -84,40 +82,38 @@ impl<'a> VideoNodeTile<'a> {
         let output_sizes = calc_chevron_sizes(self.outputs);
 
         // Construct the polygon in CCW order starting with the top left
-        let mut vertices = Vec::<Vector2<f32>>::new();
-        vertices.push(Vector2::<f32>::new(self.rect.left(), self.rect.top()));
+        let mut vertices = Vec::<Pos2>::new();
+        vertices.push(pos2(self.rect.left(), self.rect.top()));
         let mut last_loc = *self.inputs.first().unwrap_or(&f32::NAN);
         for (&loc, &size) in self.inputs.iter().zip(input_sizes.iter()) {
             assert!(loc >= last_loc, "Inputs are not sorted");
             if size > 0. {
-                vertices.push(Vector2::<f32>::new(self.rect.left(), self.rect.top() + loc - size));
-                vertices.push(Vector2::<f32>::new(self.rect.left() + size, self.rect.top() + loc));
-                vertices.push(Vector2::<f32>::new(self.rect.left(), self.rect.top() + loc + size));
+                vertices.push(pos2(self.rect.left(), self.rect.top() + loc - size));
+                vertices.push(pos2(self.rect.left() + size, self.rect.top() + loc));
+                vertices.push(pos2(self.rect.left(), self.rect.top() + loc + size));
             }
             last_loc = loc;
         }
-        vertices.push(Vector2::<f32>::new(self.rect.left(), self.rect.bottom()));
-        vertices.push(Vector2::<f32>::new(self.rect.right(), self.rect.bottom()));
+        vertices.push(pos2(self.rect.left(), self.rect.bottom()));
+        vertices.push(pos2(self.rect.right(), self.rect.bottom()));
         let mut last_loc = *self.outputs.last().unwrap_or(&f32::NAN);
         for (&loc, &size) in self.outputs.iter().rev().zip(output_sizes.iter().rev()) {
             assert!(loc <= last_loc, "Outputs are not sorted");
             if size > 0. {
-                vertices.push(Vector2::<f32>::new(self.rect.right(), self.rect.top() + loc + size));
-                vertices.push(Vector2::<f32>::new(self.rect.right() + size, self.rect.top() + loc));
-                vertices.push(Vector2::<f32>::new(self.rect.right(), self.rect.top() + loc - size));
+                vertices.push(pos2(self.rect.right(), self.rect.top() + loc + size));
+                vertices.push(pos2(self.rect.right() + size, self.rect.top() + loc));
+                vertices.push(pos2(self.rect.right(), self.rect.top() + loc - size));
             }
             last_loc = loc;
         }
-        vertices.push(Vector2::<f32>::new(self.rect.right(), self.rect.top()));
+        vertices.push(pos2(self.rect.right(), self.rect.top()));
 
         vertices.dedup_by(|a, b| (a.x - b.x).abs() < EPSILON && (a.y - b.y).abs() < EPSILON);
 
         // Push vertices of polygon
         let mut mesh = Mesh::with_texture(TextureId::default());
-        let mut points = Vec::<Pos2>::new();
-        for pos in vertices.iter() {
-            mesh.colored_vertex(pos2(pos.x, pos.y), Color32::BLACK);
-            points.push(pos2(pos.x, pos.y));
+        for &pos in vertices.iter() {
+            mesh.colored_vertex(pos, Color32::BLACK);
         }
 
         // Triangulate with ear clipping
@@ -134,7 +130,7 @@ impl<'a> VideoNodeTile<'a> {
                 let p1 = vertices[indices[ii1]];
                 let p2 = vertices[indices[ii2]];
                 let p3 = vertices[indices[ii3]];
-                let c = cross(&(p2 - p1), &(p3 - p2));
+                let c = cross(p2 - p1, p3 - p2);
                 if c > EPSILON {
                     found_exterior = true;
                 } else if c < -EPSILON {
@@ -158,6 +154,6 @@ impl<'a> VideoNodeTile<'a> {
 
         // Paint stroke
         let stroke = Stroke::new(BORDER_THICKNESS, if response.has_focus() {Color32::LIGHT_GRAY} else {Color32::GRAY});
-        ui.painter().add(Shape::closed_line(points, stroke));
+        ui.painter().add(Shape::closed_line(vertices, stroke));
     }
 }
