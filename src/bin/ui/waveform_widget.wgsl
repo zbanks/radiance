70c3c88c8c8a33601a1380d54e4cf4eb03f2f21c
Eric Van Albert <eric@van.al>
2023-03-11 00:09:21 -0500

    Render shader to widget

---
+++ src/bin/ui/waveform_widget.wgsl
@@ -0,0 +1,37 @@
+struct Uniforms {
+    resolution: vec2<f32>,
+}
+
+@group(0) @binding(0)
+var<uniform> global: Uniforms;
+
+struct VertexOutput {
+    @builtin(position) gl_Position: vec4<f32>,
+    @location(0) uv: vec2<f32>,
+};
+
+@vertex
+fn vs_main(@builtin(vertex_index) vertex_index: u32) -> VertexOutput {
+    var pos_array = array<vec2<f32>, 4>(
+        vec2<f32>(1., 1.),
+        vec2<f32>(-1., 1.),
+        vec2<f32>(1., -1.),
+        vec2<f32>(-1., -1.),
+    );
+    var uv_array = array<vec2<f32>, 4>(
+        vec2<f32>(1., 0.),
+        vec2<f32>(0., 0.),
+        vec2<f32>(1., 1.),
+        vec2<f32>(0., 1.),
+    );
+
+    return VertexOutput(
+        vec4<f32>(pos_array[vertex_index], 0., 1.),
+        uv_array[vertex_index],
+    );
+}
+
+@fragment
+fn fs_main(vertex: VertexOutput) -> @location(0) vec4<f32> {
+    return vec4<f32>(0., 0., 1., 1.);
+}
--- src/bin/ui/widgets.rs
+++ src/bin/ui/widgets.rs
@@ -1,7 +1,7 @@
 use egui::{TextureId, Vec2};
 use egui_wgpu::renderer::Renderer;
 use radiance::{ArcTextureViewSampler, AudioLevels};
-use rand::Rng;
+use std::iter;
 use std::sync::Arc;
 
 pub struct Widgets {
@@ -14,6 +14,18 @@ pub struct Widgets {
     waveform_width: u32,
     waveform_height: u32,
     waveform_texture: ArcTextureViewSampler,
+    waveform_shader_module: wgpu::ShaderModule,
+    waveform_uniform_buffer: wgpu::Buffer,
+    waveform_bind_group: wgpu::BindGroup,
+    waveform_render_pipeline: wgpu::RenderPipeline,
+}
+
+// The uniform buffer associated with the waveform
+#[repr(C)]
+#[derive(Default, Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct WaveformUniforms {
+    resolution: [f32; 2],
+    _padding: [u8; 8],
 }
 
 impl Widgets {
@@ -35,35 +47,13 @@ impl Widgets {
             sample_count: 1,
             dimension: wgpu::TextureDimension::D2,
             format: wgpu::TextureFormat::Rgba8Unorm,
-            usage: wgpu::TextureUsages::COPY_DST // XXX
+            usage: wgpu::TextureUsages::COPY_SRC
                 | wgpu::TextureUsages::RENDER_ATTACHMENT
                 | wgpu::TextureUsages::TEXTURE_BINDING,
             label: None,
         };
 
         let texture = device.create_texture(&texture_desc);
-
-        // Begin XXX
-        let random_bytes: Vec<u8> = (0..width * height * 4)
-            .map(|_| rand::thread_rng().gen::<u8>())
-            .collect();
-
-        queue.write_texture(
-            wgpu::ImageCopyTexture {
-                texture: &texture,
-                mip_level: 0,
-                origin: wgpu::Origin3d::ZERO,
-                aspect: wgpu::TextureAspect::All,
-            },
-            &random_bytes,
-            wgpu::ImageDataLayout {
-                offset: 0,
-                bytes_per_row: std::num::NonZeroU32::new(4 * width),
-                rows_per_image: std::num::NonZeroU32::new(height),
-            },
-            texture_size,
-        );
-        // End XXX
 
         let view = texture.create_view(&Default::default());
         let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
@@ -82,6 +72,105 @@ impl Widgets {
         let waveform_width = 1;
         let waveform_height = 1;
         let waveform_texture = Self::make_texture(&device, &queue, waveform_width, waveform_height);
+
+        let waveform_shader_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some(&"Waveform widget shader module"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("waveform_widget.wgsl").into()),
+        });
+
+        // The update uniform buffer for this effect
+        let waveform_uniform_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some(&"waveform widget uniform buffer"),
+            size: std::mem::size_of::<WaveformUniforms>() as u64,
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        let waveform_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: Some(&"waveform widget bind group layout"),
+                entries: &[
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0, // WaveformUniforms
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Uniform,
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    },
+                    /*wgpu::BindGroupLayoutEntry {
+                        binding: 1, // iSampler
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                        count: None,
+                    },
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 2, // iWaveformTex
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            multisampled: false,
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        },
+                        count: None,
+                    },*/
+                ],
+            });
+
+        let waveform_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &waveform_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: waveform_uniform_buffer.as_entire_binding(),
+            }],
+            label: Some("output bind group"),
+        });
+
+        let waveform_render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Waveform widget render pipeline layout"),
+                bind_group_layouts: &[&waveform_bind_group_layout],
+                push_constant_ranges: &[],
+            });
+
+        let waveform_render_pipeline =
+            device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Waveform widget render pipeline"),
+                layout: Some(&waveform_render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &waveform_shader_module,
+                    entry_point: "vs_main",
+                    buffers: &[],
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &waveform_shader_module,
+                    entry_point: "fs_main",
+                    targets: &[Some(wgpu::ColorTargetState {
+                        format: wgpu::TextureFormat::Rgba8Unorm,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleStrip,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: Some(wgpu::Face::Back),
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    unclipped_depth: false,
+                    conservative: false,
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                multiview: None,
+            });
+
         Self {
             device,
             queue,
@@ -90,6 +179,10 @@ impl Widgets {
             waveform_width,
             waveform_height,
             waveform_texture,
+            waveform_shader_module,
+            waveform_bind_group,
+            waveform_uniform_buffer,
+            waveform_render_pipeline,
         }
     }
 
@@ -99,7 +192,7 @@ impl Widgets {
         size: Vec2,
         audio: AudioLevels,
     ) -> TextureId {
-        // See if we need to remake the texture
+        // Possibly remake the texture if the size has changed
         let width = (size.x * self.pixels_per_point) as u32;
         let height = (size.y * self.pixels_per_point) as u32;
         if width != self.waveform_width || height != self.waveform_height {
@@ -107,6 +200,50 @@ impl Widgets {
             self.waveform_height = height;
             self.waveform_texture = Self::make_texture(&self.device, &self.queue, width, height);
         }
+
+        // Populate the uniforms and waveform data
+        let uniforms = WaveformUniforms {
+            ..Default::default()
+        };
+
+        self.queue.write_buffer(
+            &self.waveform_uniform_buffer,
+            0,
+            bytemuck::cast_slice(&[uniforms]),
+        );
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Waveform widget encoder"),
+            });
+        // Record output render pass.
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Output window render pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &self.waveform_texture.view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.,
+                            g: 0.,
+                            b: 0.,
+                            a: 0.,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+
+            render_pass.set_pipeline(&self.waveform_render_pipeline);
+            render_pass.set_bind_group(0, &self.waveform_bind_group, &[]);
+            render_pass.draw(0..4, 0..1);
+        }
+
+        // Submit the commands.
+        self.queue.submit(iter::once(encoder.finish()));
 
         renderer.register_native_texture(
             &self.device,
